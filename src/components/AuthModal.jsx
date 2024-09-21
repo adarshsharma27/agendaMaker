d3e4d5cfa24287cb61533e908d84f61b88e5cd3c
adarsh sharma <37371998+adarshsharma27@users.noreply.github.com>
2024-09-22 00:40:49 +0530

    Style: Modal Integration

--- src/components/AuthModal.jsx
+++ src/components/AuthModal.jsx
@@ -1,44 +1,31 @@
-import React, { useState } from "react";
-import FormField from "./FormField";
-import Login from "./Login";
+import React from "react";
+import { LuXSquare } from "react-icons/lu";
 
 const AuthModal = () => {
   return (
     <>
+      <div className="font-baijamjuree fixed top-0 left-0  flex justify-center items-center	 shadow-2xl z-99 w-full h-full bg-[#0000002b]">
+        <div className="mx-auto max-w-lg d-flex justify-center items-center bg-white rounded-lg p-4 shadow-2xl">
+          <div className="flex justify-end">
+            <LuXSquare
+              size={32}
+              className="text-gray-600 hover:scale-105 hover:text-gray-700 cursor-pointer  transition-all duration-1000 ease-in-out"
+            />
+          </div>
+          <div className="px-4 py-6">
+            <h1 className="text-center text-3xl font-bold text-rose-600 sm:text-3xl">
+              Get started today
+            </h1>
 
-      <div className="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8 font-baijamjuree">
-        <div className="mx-auto max-w-lg">
-          <h1 className="text-center text-3xl font-bold text-rose-600 sm:text-3xl">
-            Get started today
-          </h1>
-
-          <p className="mx-auto mt-4 max-w-md text-center text-gray-500 text-base">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati
-            sunt dolores deleniti inventore quaerat mollitia?
-          </p>
-
-          <form
-            action="#"
-            className="mb-0 mt-6 space-y-4 rounded-lg p-4 shadow-lg sm:p-6 lg:p-8"
-          >
-            <p className="text-center text-lg font-bold">
-              Sign in to your account
+            <p className="mx-auto mt-4 max-w-md text-center text-gray-500 text-base">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Obcaecati sunt dolores deleniti inventore quaerat mollitia?
             </p>
-
-            <button
-              type="submit"
-              className="block w-full rounded-lg bg-rose-600 px-5 py-3 text-base font-bold text-white"
-            >
-              Sign in
-            </button>
-
-            <p className="text-center text-base text-gray-500">
-              No account?
-              <a className="underline text-rose-500 pl-2" href="#" >
-                Sign up
-              </a>
+            <p className="mx-auto mt-4 max-w-md text-center text-gray-500 text-base">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Obcaecati sunt dolores deleniti inventore quaerat mollitia?
             </p>
-          </form>
+          </div>
         </div>
       </div>
     </>
