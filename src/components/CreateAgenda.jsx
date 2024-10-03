bafd58ff5567db7d5d78430f85bc3ee092515db0
adarsh sharma <37371998+adarshsharma27@users.noreply.github.com>
2024-10-03 22:40:21 +0530

    feat: Create Agenda Integration
    Validations and saving records in DB

--- src/components/CreateAgenda.jsx
+++ src/components/CreateAgenda.jsx
@@ -1,10 +1,10 @@
 import React, { useState } from "react";
 import FormField from "./FormField";
-import { LuPlusSquare } from "react-icons/lu";
+import { LuMinusSquare, LuPlusSquare } from "react-icons/lu";
 import { z } from "zod";
-import { useForm } from "react-hook-form";
+import { useFieldArray, useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
-import conf, { ID, account, databases } from "../config/config";
+import conf, { ID, databases } from "../config/config";
 import moment from "moment";
 import { useNavigate } from "react-router-dom";
 import { categoryOptions } from "../utils/categoryOptions";
@@ -17,11 +17,18 @@ const CreateAgendaSchema = z.object({
     .string()
     .min(5, { message: "Title is too short" })
     .max(20, { message: "Title is too long" }),
-  category: z.string().nonempty("Please select a category"),
+  category: z.string().min(1, "Please select a category"),
   description: z
     .string()
     .min(50, { message: "Description is too short" })
     .max(200, { message: "Description is too long" }),
+  topics: z
+    .array(
+      z.object({
+        name: z.string().min(1, { message: "Topic name is required" }),
+      })
+    )
+    .min(1, { message: "At least one topic is required" }),
 });
 const CreateAgenda = () => {
   const navigate = useNavigate();
@@ -31,7 +38,18 @@ const CreateAgenda = () => {
     watch,
     formState: { errors },
     reset,
-  } = useForm({ resolver: zodResolver(CreateAgendaSchema) });
+    control,
+  } = useForm({
+    defaultValues: {
+      title: "",
+      label: "",
+      category: "",
+      topics: [{ name: "" }],
+      description: "",
+    },
+
+    resolver: zodResolver(CreateAgendaSchema),
+  });
 
   // Watch the input values
 
@@ -40,20 +58,36 @@ const CreateAgenda = () => {
     !watch("label") ||
     !watch("category") ||
     !watch("description");
-  const handleCreateAgenda = async (data) => {
-    const { email, password } = data;
-  };
-  const [topic, setTopic] = useState("");
-  const [allTopics, setAllTopics] = useState([]);
 
-  const handleChangeTopic = (e) => {
-    setTopic(e.target.value);
+  const { fields, append, remove } = useFieldArray({
+    control,
+    name: "topics",
+  });
+  const handleCreateAgenda = async (data) => {
+    const { title, label, category, topics, description } = data;
+    const createdAt = moment(new Date()).format("MMM Do YYYY, h:mm:ss a");
+    const topicsArray = topics.map((topic) => topic.name);
+    const payload = {
+      title,
+      label,
+      category,
+      description,
+      topics: topicsArray,
+      createdAt,
+    };
+    try {
+      await databases.createDocument(
+        conf.databaseId,
+        conf.collectionId,
+        ID.unique(),
+        payload
+      );
+      console.log("Agenda Created Successfully");
+    } catch (error) {
+      console.log(error.message);
+    }
   };
-  const handleAddTopic = () => {
-    setAllTopics([...allTopics, topic]);
-  };
-  console.log(topic);
-  console.log(allTopics, "all");
+
   return (
     <>
       <div className="mx-auto max-w-screen-xl px-4 py-6 sm:px-6 lg:px-8 font-baijamjuree">
@@ -63,45 +97,24 @@ const CreateAgenda = () => {
           </h1>
           <div className="mb-0 mt-6 space-y-4 rounded-lg p-4 shadow-lg sm:p-6 lg:p-8">
             <form onSubmit={handleSubmit(handleCreateAgenda)}>
-              <FormField
-                label="Title"
-                name="title"
-                placeholder="Please Enter Title"
-                type="text"
-                // value={formData?.title}
-                // handleChange={handleChange}
-                register={register}
-                error={errors.title}
-              />
-              <FormField
-                label="Label"
-                name="label"
-                placeholder="Please Enter Label"
-                type="text"
-                // value={formData?.label}
-                // handleChange={handleChange}
-                register={register}
-                error={errors.label}
-              />
-              <div className="relative">
+              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4">
                 <FormField
-                  label="Topic"
-                  name="topic"
-                  placeholder="Please Enter Topic"
+                  label="Title"
+                  name="title"
+                  placeholder="Please Enter Title"
                   type="text"
-                  // value={topic}
-                  // handleChange={handleChangeTopic}
                   register={register}
-                  error={errors.topic}
+                  error={errors.title}
                 />
-
-                <LuPlusSquare
-                  size={30}
-                  className="text-rose-600 absolute right-4 top-8 cursor-pointer"
-                  onClick={handleAddTopic}
+                <FormField
+                  label="Label"
+                  name="label"
+                  placeholder="Please Enter Label"
+                  type="text"
+                  register={register}
+                  error={errors.label}
                 />
               </div>
-
               <FormField
                 label="Category"
                 name="category"
@@ -111,6 +124,51 @@ const CreateAgenda = () => {
                 error={errors.category}
                 options={categoryOptions}
               />
+              <div>
+                <label
+                  className="font-semibold text-gray-500 font-baijamjuree"
+                  htmlFor="topics"
+                >
+                  Please Enter Topic
+                </label>
+
+                {fields.map((field, index) => (
+                  <div key={field.id} className="relative">
+                    <input
+                      className="w-full rounded-lg text-gray-600 border-gray-200 p-4 pe-12 text-base font-baijamjuree shadow-sm"
+                      placeholder="Please Enter Topic"
+                      type="text"
+                      {...register(`topics.${index}.name`)}
+                    />
+                    {fields.length > 1 && (
+                      <LuMinusSquare
+                        size={24}
+                        className="text-blue-500 absolute right-4 top-4 cursor-pointer"
+                        onClick={() => remove(index)}
+                      />
+                    )}
+                    {errors?.topics && (
+                      <span className="text-red-400 py-2 font-semibold">
+                        {errors?.topics[index]?.name?.message}
+                      </span>
+                    )}
+                  </div>
+                ))}
+                <div className="py-2">
+                  <button
+                    type="button"
+                    className="flex items-center gap-1 md:w-1/6    rounded px-6 py-3 text-base font-bold text-gray-600 shadow hover:text-rose-500 focus:outline-none focus:ring active:text-rose-500"
+                    onClick={() => append({ name: "" })}
+                  >
+                    <LuPlusSquare
+                      size={20}
+                      className="text-rose-600  cursor-pointer"
+                    />
+                    Add Topics
+                  </button>
+                </div>
+              </div>
+
               <FormField
                 label="Description"
                 name="description"
@@ -136,7 +194,8 @@ const CreateAgenda = () => {
                     errors.title ||
                     errors.label ||
                     errors.category ||
-                    errors.description
+                    errors.description ||
+                    errors.topics
                   }
                 >
                   Create
